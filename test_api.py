7742a6aa3012426ee3233bf861555e139ceac8f9
Shakkaw <the.andre.lopes@gmail.com>
2022-02-18 19:40:30 +0000

    Start creating a CLI interface for API call

--- test_api.py
+++ test_api.py
@@ -2,36 +2,77 @@
 
 import requests
 
-url = 'http://192.168.1.7:5000/' #your server url here
+url = 'http://192.168.1.7:5000/' #the server url here, in this case is the local ip with the port
 
-data = {
-    'duedate': 'September 1, 2014',
-    'from_addr': {
-        'addr1': 'Hamilton, New York',
-        'addr2': 'Sunnyville, CA 12345',
-        'company_name': 'Python Tip'
-    },
-    'invoice_number': 156,
-    'items': [{
-            'charge': 500.0,
-            'title': 'Brochure design'
-        },
-        {
-            'charge': 85.0,
-            'title': 'Hosting (6 months)'
-        },
-        {
-            'charge': 10.0,
-            'title': 'Domain name (1 year)'
-        }
-    ],
-    'to_addr': {
-        'company_name': 'hula hoop',
-        'client_email': 'john@example.com',
-        'client_name': 'Yasoob Khalid'
-    }
+
+#below is a example of all the items available thru the API
+
+# data = {
+#     'duedate': '1 September, 2014',
+#     'from_addr': {
+#         'company_name': 'Company ABC',
+#         'addr1': 'Hamilton, New York',
+#         'addr2': 'Sunnyville, CA 12345'
+#     },
+#     'invoice_number': 156,
+#     'items': [{
+#             'charge': 500.0,
+#             'title': 'Brochure design'
+#         },
+#         {
+#             'charge': 85.0,
+#             'title': 'Hosting (6 months)'
+#         },
+#         {
+#             'charge': 10.0,
+#             'title': 'Domain name (1 year)'
+#         }
+#     ],
+#     'to_addr': {
+#         'company_name': 'Company DEF',
+#         'client_name': 'John Smith',
+#         'client_email': 'johnsmith@companydef.com'
+#     }
+# }
+
+print("Fill the information of the invoice\n\n")
+
+
+invoice_number = 1  #* TODO: make this a commulative number  
+duedate = input("\nDue date: (day month, year)")
+
+from_addr = {
+    'company_name': input("\nFrom: "),
+    'addr1': input("\nAddress Line 1: (Street, Number)"),
+    'addr2': input("\nAddress Line 2: (Postal-Code City)")
 }
 
-html = requests.post(url, json=data)
-with open('invoice.pdf', 'wb') as f:
-    f.write(html.content)
+to_addr = {
+    'company_name': input("\nTo: "),
+    'client_name': input("\nClient Name: "),
+    'client_email': input("\nClient Email: ")
+}
+
+items = []
+
+print("Add the items to bill")
+answer = input("Add another item?")
+if answer:
+    pass
+
+#! TODO: FINISH THIS PART!!
+
+user_data = {
+    'duedate': duedate,
+
+}
+
+
+
+
+try: 
+    html = requests.post(url, json=user_data)
+    with open('invoice.pdf', 'wb') as f:
+        f.write(html.content)
+except:
+    print("Connection failed")
