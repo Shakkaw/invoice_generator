3aed27f00c96ccf57472044332f5b9f7bfb3cca7
Shakkaw <the.andre.lopes@gmail.com>
2022-02-19 16:13:34 +0000

    Add timestamp to output + sintax cleanup

--- test_api.py
+++ test_api.py
@@ -1,11 +1,12 @@
 # this script is meant to test the API to request a invoice
 
 import requests
+from datetime import datetime
 
-url = 'http://192.168.1.7:5000/' #the server url here, in this case is the local ip with the port
+url = "http://192.168.1.7:5000/"  # the server url here, in this case is the local ip with the port
 
 
-#below is a example of all the items available thru the API
+# below is a example of all the items available thru the API
 
 # data = {
 #     'invoice_number': 111,
@@ -18,7 +19,7 @@ url = 'http://192.168.1.7:5000/' #the server url here, in this case is the local
 #     'items': [{
 #             'title': 'Homepage w/ host+domain',
 #             'charge': 400.50
-            
+
 #         },
 #         {
 #             'title': 'Hardware setup',
@@ -39,49 +40,50 @@ url = 'http://192.168.1.7:5000/' #the server url here, in this case is the local
 print("Fill the information of the invoice\n\n")
 
 
-invoice_number = 55  #* TODO: make this a commulative number  
+invoice_number = 1  # * TODO: make this a commulative number
 duedate = input("\nDue date (day month, year):  ")
 
 from_addr = {
-    'company_name': input("\nFrom: "),
-    'addr1': input("\nAddress Line 1 (Street, Number):  "),
-    'addr2': input("\nAddress Line 2 (Postal-Code City):  ")
+    "company_name": input("\n\nFrom: "),
+    "addr1": input("\nAddress Line 1 (Street , Number):  "),
+    "addr2": input("\nAddress Line 2 (Postal-Code City):  "),
 }
 
 to_addr = {
-    'company_name': input("\n\t\t\t\t\tTo:  "),
-    'client_name':  input("\n\t\t\t\t\tClient Name:  "),
-    'client_email': input("\n\t\t\t\t\tClient Email:  ")
+    "company_name": input("\n\t\t\t\t\t\t\tTo:  "),
+    "client_name": input("\n\t\t\t\t\t\t\tClient Name:  "),
+    "client_email": input("\n\t\t\t\t\t\t\tClient Email:  "),
 }
 
 items = []
 
 
-print("\n\nAdd the items to bill")
+print("\n\nAdd the items to charge")
 
-add_more = 'y'
+add_more = "y"
 
-while add_more == 'y':
+while add_more == "y":
     item = {}
-    item['title'] = str(input("\nItem name:  "))
-    item['charge'] = float(input("\nPrice:  "))
+    item["title"] = str(input("\nItem name:  "))
+    item["charge"] = float(input("\nPrice:  "))
 
     items.append(item)
     add_more = str(input("\nAdd another item? [y/n]  "))
 
 
 user_data = {
-    'duedate': duedate,
-    'invoice_number': invoice_number,
-    'from_addr': from_addr,
-    'to_addr': to_addr,
-    'items': items
+    "duedate": duedate,
+    "invoice_number": invoice_number,
+    "from_addr": from_addr,
+    "to_addr": to_addr,
+    "items": items,
 }
 
 
-try: 
+try:
+    today = datetime.today().strftime("%-d_%B_%Y")
     html = requests.post(url, json=user_data)
-    with open('invoice.pdf', 'wb') as f:
+    with open(f"invoice_{today}.pdf", "wb") as f:
         f.write(html.content)
 except:
     print("Connection failed")
