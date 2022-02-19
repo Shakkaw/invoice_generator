a9f60f5790b8699a71d4ce00e6a886e0695bc02c
Shakkaw <the.andre.lopes@gmail.com>
2022-02-19 16:12:19 +0000

    File reformat and cleanup

--- generate_invoice.py
+++ generate_invoice.py
@@ -7,7 +7,8 @@ import os
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET', 'POST'])
+
+@app.route("/", methods=["GET", "POST"])
 def invoice_template():
 
     posted_data = request.get_json() or {}
@@ -15,58 +16,54 @@ def invoice_template():
     today = datetime.today().strftime("%-d %B , %Y")
 
     default_data = {
-        'duedate': "28 Fevereiro, 2022",
-        'from_addr': {
-            'company_name': 'André Lopes LDA.',
-            'addr1': 'Rua 1º de Maio, 44-A',
-            'addr2': '6300-111 Guarda' 
+        "duedate": "28 Fevereiro, 2022",
+        "from_addr": {
+            "company_name": "André Lopes LDA.",
+            "addr1": "Rua 1º de Maio, 44-A",
+            "addr2": "6300-111 Guarda",
         },
-        'invoice_number': 1,
-        'items': [
-            {
-            'title': 'Montagem servidor',
-            'charge': 450.00
-            },
-            {
-            'title': 'Configuração servidor',
-            'charge': 150.00 
-            },
-            {
-            'title': 'Website c/ domínio',
-            'charge': 80.00
-            }
+        "invoice_number": 1,
+        "items": [
+            {"title": "Montagem servidor", "charge": 450.00},
+            {"title": "Configuração servidor", "charge": 150.00},
+            {"title": "Website c/ domínio", "charge": 80.00},
         ],
-        'to_addr': {
-            'company_name': 'Empresa ABC',
-            'client_name': 'José Augusto',
-            'client_email': 'jaugusto@mail.com'
-        }
+        "to_addr": {
+            "company_name": "Empresa ABC",
+            "client_name": "José Augusto",
+            "client_email": "jaugusto@mail.com",
+        },
     }
-    
-    duedate = posted_data.get('duedate', default_data['duedate'])
-    from_addr = posted_data.get('from_addr', default_data['from_addr'])
-    to_addr = posted_data.get('to_addr', default_data['to_addr'])
-    invoice_number = posted_data.get('invoice_number', default_data['invoice_number'])
-    items = posted_data.get('items', default_data['items'])
-    
-    total = sum([i['charge'] for i in items])
 
-    rendered = render_template('invoice.html', 
-                            date = today,
-                            from_addr = from_addr,
-                            to_addr = to_addr,
-                            items = items,
-                            total = total,
-                            invoice_number = invoice_number,
-                            duedate = duedate)
+    duedate = posted_data.get("duedate", default_data["duedate"])
+    from_addr = posted_data.get("from_addr", default_data["from_addr"])
+    to_addr = posted_data.get("to_addr", default_data["to_addr"])
+    invoice_number = posted_data.get("invoice_number", default_data["invoice_number"])
+    items = posted_data.get("items", default_data["items"])
+
+    total = sum([i["charge"] for i in items])
+
+    rendered = render_template(
+        "invoice.html",
+        date=today,
+        from_addr=from_addr,
+        to_addr=to_addr,
+        items=items,
+        total=total,
+        invoice_number=invoice_number,
+        duedate=duedate,
+    )
 
     html = HTML(string=rendered)
 
-    rendered_pdf = html.write_pdf() # enter the relative path here if you want to save the invoice instead of sending it, 
-                                    # be aware that will make the webpage show a "Could not load the PDF" error
+    rendered_pdf = (
+        html.write_pdf()
+    )  # enter the relative path here if you want to save the invoice instead of sending it,
+    # be aware that will make the webpage show a "Could not load the PDF" error
 
-    return send_file(io.BytesIO(rendered_pdf), download_name='invoice.pdf')
+    return send_file(io.BytesIO(rendered_pdf), download_name="invoice.pdf")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host="0.0.0.0", port=port)
