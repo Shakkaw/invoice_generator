73976b7324e43867bd5c3e786b6d4955c1b53291
Shakkaw <the.andre.lopes@gmail.com>
2022-02-19 15:48:00 +0000

    Finish CLI to input data for API request

--- test_api.py
+++ test_api.py
@@ -8,66 +8,75 @@ url = 'http://192.168.1.7:5000/' #the server url here, in this case is the local
 #below is a example of all the items available thru the API
 
 # data = {
-#     'duedate': '1 September, 2014',
+#     'invoice_number': 111,
+#     'duedate': 'day Month, year',
 #     'from_addr': {
-#         'company_name': 'Company ABC',
-#         'addr1': 'Hamilton, New York',
-#         'addr2': 'Sunnyville, CA 12345'
+#         'company_name': 'Company name',
+#         'addr1': 'Street name, Number',
+#         'addr2': 'Postal Code, City'
 #     },
-#     'invoice_number': 156,
 #     'items': [{
-#             'charge': 500.0,
-#             'title': 'Brochure design'
+#             'title': 'Homepage w/ host+domain',
+#             'charge': 400.50
+            
 #         },
 #         {
-#             'charge': 85.0,
-#             'title': 'Hosting (6 months)'
+#             'title': 'Hardware setup',
+#             'charge': 200.0
 #         },
 #         {
-#             'charge': 10.0,
-#             'title': 'Domain name (1 year)'
+#             'title': 'Network setup',
+#             'charge': 150.75
 #         }
 #     ],
 #     'to_addr': {
-#         'company_name': 'Company DEF',
-#         'client_name': 'John Smith',
-#         'client_email': 'johnsmith@companydef.com'
+#         'company_name': 'Client Company Name',
+#         'client_name': 'Client Name',
+#         'client_email': 'client@mail.com'
 #     }
 # }
 
 print("Fill the information of the invoice\n\n")
 
 
-invoice_number = 1  #* TODO: make this a commulative number  
-duedate = input("\nDue date: (day month, year)")
+invoice_number = 55  #* TODO: make this a commulative number  
+duedate = input("\nDue date (day month, year):  ")
 
 from_addr = {
     'company_name': input("\nFrom: "),
-    'addr1': input("\nAddress Line 1: (Street, Number)"),
-    'addr2': input("\nAddress Line 2: (Postal-Code City)")
+    'addr1': input("\nAddress Line 1 (Street, Number):  "),
+    'addr2': input("\nAddress Line 2 (Postal-Code City):  ")
 }
 
 to_addr = {
-    'company_name': input("\nTo: "),
-    'client_name': input("\nClient Name: "),
-    'client_email': input("\nClient Email: ")
+    'company_name': input("\n\t\t\t\t\tTo:  "),
+    'client_name':  input("\n\t\t\t\t\tClient Name:  "),
+    'client_email': input("\n\t\t\t\t\tClient Email:  ")
 }
 
 items = []
 
-print("Add the items to bill")
-answer = input("Add another item?")
-if answer:
-    pass
 
-#! TODO: FINISH THIS PART!!
+print("\n\nAdd the items to bill")
+
+add_more = 'y'
+
+while add_more == 'y':
+    item = {}
+    item['title'] = str(input("\nItem name:  "))
+    item['charge'] = float(input("\nPrice:  "))
+
+    items.append(item)
+    add_more = str(input("\nAdd another item? [y/n]  "))
+
 
 user_data = {
     'duedate': duedate,
-
+    'invoice_number': invoice_number,
+    'from_addr': from_addr,
+    'to_addr': to_addr,
+    'items': items
 }
-
-
 
 
 try: 
